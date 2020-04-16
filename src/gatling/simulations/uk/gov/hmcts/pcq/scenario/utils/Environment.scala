4cd46c6ccf303df1aeefb8c69395ed0b30ae6c45
jmcadam <mcadam88@gmail.com>
2020-04-16 10:27:19 +0100

    initial commit

---
+++ src/gatling/simulations/uk/gov/hmcts/pcq/scenario/utils/Environment.scala
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.perfproject.scenario.utils
+
+import io.gatling.http.Predef.http
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+
+object Environment {
+
+  /** put all the required environment variables and make sure name is matching with jenkinsfile_nightly
+
+      please refere example project https://github.com/hmcts/probate-performance-case-worker
+
+  https://build.platform.hmcts.net/job/HMCTS_Nightly_Probate/job/probate-performance-case-worker/  */
+}
---
+++ src/gatling/simulations/uk/gov/hmcts/pcq/simulations/checks/CurrentPageUrl.scala
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.perfproject.simulations.checks
+
+import io.gatling.core.Predef._
+import io.gatling.core.check.CheckBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.check.HttpCheck
+import jodd.lagarto.dom.NodeSelector
+import io.gatling.http.check.url.CurrentLocationCheckType
+import io.gatling.core.check.extractor.css.CssCheckType
+
+object CurrentPageUrl {
+  def save: CheckBuilder[CurrentLocationCheckType,String,String] = currentLocation.saveAs("currentPageUrl")
+
+  def currentPageTemplate: String = "${currentPageUrl}"
+}
